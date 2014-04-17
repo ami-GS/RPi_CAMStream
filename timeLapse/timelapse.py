dba4d82e6f706ba15df4ea3b9972a5eb27e0c322
Daiki Aminaka <1991.daiki@gmail.com>
2014-04-17 17:21:17 +0900

    removed bugs to make mp4

--- timeLapse/timelapse.py
+++ timeLapse/timelapse.py
@@ -1,62 +1,38 @@
-import time
 import cv2
 from datetime import datetime
-import os, subprocess
+import os
+import subprocess
+import cameraset
 
-WIDTH = 480
-HEIGHT = 360
+WIDTH = 860 # 2592 max
+HEIGHT = 720 # 1944 max
 DIRNAME = "TL_%s" % datetime.now().strftime("%Y%m%d-%H%M%S")
 ZFILL = 7
-class Camera(object):
-    def __init__(self):
-        self.num = 1
-
-    def takeImage(self):
-        pass
-
-    def timeStamp(self):
-        stamp = str(self.num).zfill(ZFILL)
-        self.num += 1
-        return stamp
-
-class usbCamera(Camera):
-    def __init__(self):
-        super(usbCamera, self).__init__()
-        self.camera = cv2.VideoCapture(0)
-
-    def takeImage(self):
-        _, img = self.camera.read()
-        if _:
-            cv2.imwrite("./%s/%s.jpg" % (DIRNAME, self.timeStamp()), img)
-
-class piCamera(Camera):
-    def __init__(self):
-        super(piCamera, self).__init__()
-        import picamera
-        self.camera = picamera.PiCamera()
-        self.camera.resolution = (WIDTH, HEIGHT)
-        time.sleep(2)
-
-    def takeImage(self):
-        self.camera.capture("./%s/%s.jpg" % (DIRNAME, self.timeStamp()))
 
 def makeVideo(FPS):
     os.chdir(DIRNAME)
-    subprocess.call("ffmpeg -r "+str(FPS)+" -i %07d.jpg -vcodec libx264 -sameq -vf 'scale=1620:1080,pad=1920:1080:150:0,setdar=16:9' "+"%s-%dfps.mp4"
-                    % (DIRNAME, FPS))
-
+    print(["ffmpeg", "-r", "%d" % FPS, "-i", "%%0%dd.jpg" % ZFILL,
+                     "-vcodec", "libx264", "-qscale", "0", "-vf",
+                     "'scale=1620:1080,pad=1920:1080:150:0,setdar=16:9'",
+                     "%s-%dfps.mp4" % (DIRNAME, FPS)])
+    subprocess.call(["ffmpeg", "-r", "%d" % FPS, "-i", "%%0%dd.jpg" % ZFILL,
+                     "-vcodec", "libx264", "-q", "0", "-vf",
+                     "scale=1620:1080,pad=1920:1080:150:0,setdar=16:9",
+                     "%s-%dfps.mp4" % (DIRNAME, FPS)])
 def mainLoop(camera, FPS):
     while True:
         print camera.num
         camera.takeImage()
-        if cv2.waitKey(int(1000/FPS)) == 27:
+        if cv2.waitKey(int(1000/FPS)) == 27 or camera.num == 100:
+            camera.terminalte()
+            makeVideo(FPS)
             break
 
 def main():
     try:
-        camera = piCamera()
+        camera = cameraset.piCamera(DIRNAME, ZFILL, WIDTH, HEIGHT)
     except:
-        camera = usbCamera()
+        camera = cameraset.usbCamera(DIRNAME, ZFILL, WIDTH, HEIGHT)
     while True:
         try:
             FPS = float(raw_input("Input FPS to start >> "))
